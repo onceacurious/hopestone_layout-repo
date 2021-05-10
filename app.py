6ed9a7ad616dbe4fe9a26c3651def2705ff33662
onceacurious <onceacurious@gmail.com>
2021-05-11 00:19:08 +0800

    Set up New Pages
    Set up register, service, and validators using wtf.

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from data import Services, Works
 
 
 app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:*#4Kgdngf$KTGM@localhost/hopestone'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    userName = db.Column(db.String(15), unique=True)
+    password = db.Column(db.String(15))
+    confirm = db.Column(db.String(15))
+    email = db.Column(db.String(50))
+    contact = db.Column(db.String(15))
+
+    def __init__(self, name, userName, password, email, contact):
+        self.name = name
+        self.userName = userName
+        self.password = password
+        self.email = email
+        self.contact = contact
+
+
+Services = Services()
+Works = Works()
 
 
 @app.route('/')
@@ -15,10 +54,40 @@ def contact_page():
 
 
 @app.route('/work')
-def work_page():
-    return render_template('work.html')
+def works():
+    return render_template('work.html', works=Works)
 
 
 @app.route('/services')
-def services_page():
-    return render_template('services.html')
+def services():
+    return render_template('services.html', services=Services)
+
+
+@app.route('/service/<string:title>/')
+def service(title):
+    return render_template(
+        'service.html',
+        title=title
+    )
+
+
+class RegisterFrom(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    userName = StringField('Username', [
+        validators.Length(min=3, max=15)
+    ])
+    password = PasswordField('Password', [
+        validators.EqualTo('confirm', message='Password do not match')
+    ])
+    confirm = PasswordField('Confrim Password')
+    email = StringField('Email Address', [validators.Length(min=6, max=50)])
+    contact = StringField('Contact', [validators.Length(min=7, max=15)])
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterFrom(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html')
+
+    return render_template('register.html', form=form)
---
+++ data.py
@@ -0,0 +1,80 @@
+def Services():
+    services = [
+        {
+            'id': 1,
+            'file_name': '001.png',
+            'title': 'Development',
+            'body': '1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'published_date': '01-01-2021'
+        },
+        {
+            'id': 2,
+            'file_name': '002.png',
+            'title': 'SEO',
+            'body': '2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'published_date': '02-01-2021'
+        },
+        {
+            'id': 3,
+            'file_name': '003.png',
+            'title': 'Marketing',
+            'body': '3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'published_date': '03-01-2021'
+        },
+        {
+            'id': 4,
+            'file_name': '004.png',
+            'title': 'CRM',
+            'body': '4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'published_date': '04-01-2021'
+        },
+        {
+            'id': 5,
+            'file_name': '005.png',
+            'title': 'App Dev',
+            'body': '5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'published_date': '04-01-2021'
+        },
+        {
+            'id': 6,
+            'file_name': '006.png',
+            'title': 'Debugging',
+            'body': '6 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'published_date': '04-01-2021'
+        }
+    ]
+    return services
+
+
+def Works():
+    works = [
+        {
+            'id': 1,
+            'title': "Work One",
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'finished_date': '01-01-2021',
+            'published_date': '01-05-2021'
+        },
+        {
+            'id': 2,
+            'title': "Work Two",
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'finished_date': '01-01-2021',
+            'published_date': '01-05-2021'
+        },
+        {
+            'id': 3,
+            'title': "Work Three",
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'finished_date': '01-01-2021',
+            'published_date': '01-05-2021'
+        },
+        {
+            'id': 4,
+            'title': "Work Four",
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eos.',
+            'finished_date': '01-01-2021',
+            'published_date': '01-05-2021'
+        }
+    ]
+    return works
